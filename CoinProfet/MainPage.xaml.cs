82585888ba43774cf102ce5f46bd18ad76220932
Ben <ben399399@naver.com>
2018-01-15 10:14:14 +0900

    git init

---
+++ CoinProfet/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using HtmlAgilityPack;
+using System.Diagnostics;
+using System.Threading.Tasks;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CoinProfet
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();  
+        }
+
+        string currentDateFormatter()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd");
+        }
+        string UpBitAPIgenerator(Coin.CoinType coin,string duration, string currentdate)
+        {
+            //label.Text = currentDateFormatter();
+            return "https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/" + duration + "?code=CRIX.UPBIT.KRW-" + coin.ToString() + "&count=1";
+        }
+        void LoadWebAsync()
+        {
+            string Link = UpBitAPIgenerator(Coin.CoinType.BTC, "10", currentDateFormatter());
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(Link);
+            string labelText = "null";
+            try { 
+                HtmlNodeCollection nodecollection = doc.DocumentNode.SelectNodes("/html/body/pre");
+            }
+            catch (NullReferenceException) { 
+            }
+            label.Text = labelText;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadWebAsync();
+        }
+    }
+}
---
+++ CoinProfet/coin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinProfet
+{
+    class Coin
+    {
+        public enum CoinType
+        {
+            SNT,ETC,NEO,BCC,ETH,
+            ZEC,XMR,BTC,BTG,WAVES,
+            QTUM,LSK,STRAT,LTC,DASH,
+            XRP,VTC,XLM,PIVX,ARK,
+            XEM,STEEM,OMG,REP,MTL,
+            ADA,GRS,SBD,KMD,STORJ,
+            MER,POWR,EMC2,TIX
+        }
+    }
+}
